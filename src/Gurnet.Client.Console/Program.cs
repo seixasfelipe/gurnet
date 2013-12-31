e645ff732a76a94d44ec7cf9cbaf270b1d077d5d
Felipe Seixas <seixasfelipe@gmail.com>
2013-12-31 18:44:06 -0500

    initial commit

---
+++ src/Gurnet.Client.Console/Program.cs
@@ -0,0 +1,50 @@
+﻿using Lidgren.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gurnet.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting client...");
+
+            var config = new NetPeerConfiguration("gurnet");
+
+            NetClient client = new NetClient(config);            
+
+            string input;
+            while (!(input = Console.ReadLine()).Equals("quit", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine("[d]{0}", input);
+
+                if (input.Equals("connect", StringComparison.InvariantCultureIgnoreCase)
+                    && client.ConnectionStatus == NetConnectionStatus.Disconnected)
+                {
+                    client.Start();
+                    NetOutgoingMessage hailMsg = client.CreateMessage("Hail server");
+                    client.Connect("localhost", 14242, hailMsg);
+
+                    Console.WriteLine("[d]connecting...");
+                }
+                else
+                {
+                    NetOutgoingMessage outMsg = client.CreateMessage(input);
+                    client.SendMessage(outMsg, NetDeliveryMethod.ReliableOrdered);
+                    client.FlushSendQueue();
+                    Console.WriteLine("[d]sending msg [{0}]", input);
+                }
+            }
+
+            if (client.ConnectionStatus == NetConnectionStatus.Connected)
+            {
+                client.Disconnect("Disconnect requested by user");
+            }
+
+        }
+    }
+}
---
+++ src/Gurnet.Server.Console/Program.cs
@@ -0,0 +1,76 @@
+﻿using Lidgren.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gurnet.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting server...");
+
+            var config = new NetPeerConfiguration("gurnet");
+            config.Port = 14242;
+
+            var server = new NetServer(config);
+            server.Start();
+
+            ConsoleKeyInfo key;
+            while(true) 
+            {
+                if (Console.KeyAvailable)
+                {
+                    key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.Q)
+                    {
+                        server.Shutdown("Shutdown requested by user");
+                        break;
+                    }
+                }
+
+                NetIncomingMessage inMsg;
+                string text;
+                while ((inMsg = server.ReadMessage()) != null)
+                {
+                    // handle incoming message
+                    switch (inMsg.MessageType)
+                    {
+                        case NetIncomingMessageType.DebugMessage:
+                        case NetIncomingMessageType.ErrorMessage:
+                        case NetIncomingMessageType.WarningMessage:
+                        case NetIncomingMessageType.VerboseDebugMessage:
+                            text = inMsg.ReadString();
+                            Console.WriteLine(text);
+                            break;
+                        case NetIncomingMessageType.StatusChanged:
+                            NetConnectionStatus status = (NetConnectionStatus)inMsg.ReadByte();
+							string reason = inMsg.ReadString();
+							Console.WriteLine(NetUtility.ToHexString(inMsg.SenderConnection.RemoteUniqueIdentifier) + " " + status + ": " + reason);
+                            break;
+                        case NetIncomingMessageType.Data:
+                            text = string.Format("{0} said: {1}", NetUtility.ToHexString(inMsg.SenderConnection.RemoteUniqueIdentifier), inMsg.ReadString());
+                            Console.WriteLine(text);
+
+                            foreach (var con in server.Connections)
+                            {
+                                var outMsg = server.CreateMessage(text);
+                                con.SendMessage(outMsg, NetDeliveryMethod.ReliableOrdered, 0);
+                            }
+
+                            break;
+                        default:
+                            Console.WriteLine("Unhandled type: " + inMsg.MessageType + " " + inMsg.LengthBytes + " bytes " + inMsg.DeliveryMethod + "|" + inMsg.SequenceChannel);
+                            break;
+                    }
+                }
+
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
