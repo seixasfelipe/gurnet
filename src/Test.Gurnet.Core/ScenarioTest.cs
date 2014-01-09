45a2e3eb52193bbe33243eef1e6b61e0236f85e8
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2014-01-08 23:34:31 -0500

    Implements TestScenarioRespawnPositions

--- src/Test.Gurnet.Core/ScenarioTest.cs
+++ src/Test.Gurnet.Core/ScenarioTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Gurnet.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,13 @@ namespace Test.Gurnet.Core
         [TestMethod]
         public void TestScenarioRespawnPositions()
         {
-            // TODO: Must validate if all respawn positions are inside the map
+            Position expectedFirstPosition = new Position { X = 1, Y = 1};
+            Position expectedSecondPosition = new Position { X = 2, Y = 2 };
+            
+            Scenario scenario = new Scenario(2, 2, new List<Position> { new Position { X = 1, Y = 1 }, new Position { X = 2, Y = 2 } });
+
+            Assert.AreEqual(expectedFirstPosition, scenario.GetNextRespawnPosition());
+            Assert.AreEqual(expectedSecondPosition, scenario.GetNextRespawnPosition());
         }
     }
 }
