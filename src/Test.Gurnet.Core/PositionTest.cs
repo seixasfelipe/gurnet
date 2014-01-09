8fbc4df2438031a8966cc3b1378bcd9371a9cab7
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2014-01-08 23:39:40 -0500

    implements PositionTest

---
+++ src/Test.Gurnet.Core/PositionTest.cs
@@ -0,0 +1,35 @@
+﻿using Gurnet.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Gurnet.Core
+{
+    [TestClass]
+    public class PositionTest
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            Position position = new Position();
+            Assert.IsNotNull(position);
+        }
+
+        [TestMethod]
+        public void TestXProperty()
+        {
+            Position position = new Position { X = 1 };
+            Assert.AreEqual(1, position.X);
+        }
+
+        [TestMethod]
+        public void TestYProperty()
+        {
+            Position position = new Position { Y = 1 };
+            Assert.AreEqual(1, position.Y);
+        }
+    }
+}
